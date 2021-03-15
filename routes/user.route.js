1104b477c3a1dd8723ae43e655ddfdbb43d5371b
suhanathan <suhasini_95@hotmail.com>
2021-03-15 22:09:51 +0800

    got the authentication and server working

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,43 @@
 const router = require('express').Router()
+const User = require('../models/account.model')
 
-router.get("/",(req,res)=>{
-    res.send("default route!!")
+router.get("/",async(req,res)=>{
+    let tempUser = await res.locals.currentUser._id
+    console.log("temp user is ", tempUser)
+    res.redirect(`/user/${tempUser}`)
+})
+
+//where the user can see his/her todolist
+router.get("/user/:id",async(req,res)=>{
+    try{
+        console.log(req.params.id)
+        let user = await User.findById(req.params.id)
+        console.log("user is",user)
+        res.render("todo/main",user)
+    }catch(e){
+
+    }
+})
+
+//user creates his/her todolist
+router.get("/user/:id/create",async (req,res)=>{
+    res.render("todo/create")
+})
+
+
+router.put("/user/:id/create",async (req,res)=>{
+    try{
+        let {title,description,isCompleted } = req.body
+        let tempToDo = {
+            title,
+            description,
+            isCompleted
+        }
+        let user = await User.findByIdAndUpdate(req.params.id,{$push:{todo:tempToDo}})
+        res.redirect(`/user/${req.params.id}`)
+    }catch(e){
+        console.log(e)
+    }
 })
 
 
