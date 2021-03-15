281d56e1ecb9493546a671b39fc52b8afe10006f
watanaprasit <watanaprasit@gmail.com>
2021-03-15 15:33:13 +0800

    models

---
+++ models/account.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+const userSchema = new Schema ({
+    email: { type: String, required: true},
+    password: { type: String, required: true}
+})
+
+userSchema.pre("save", async function(next){
+    let user = this
+    if(!user.isModified("password")) return next()
+    let hashedPassword = await bcrypt.hash(user.password, 10)
+    user.password = hashedPassword
+    next()
+})
+
+module.exports = mongoose.model("User", userSchema)
